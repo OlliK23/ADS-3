bf02c1c18975fdf4fda8bee99f526280da1fcfea
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-03-24 19:06:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int l = 0;
+  int r = size - 1;
+  int count = 0;
+  while (l <= r) {
+    int mid = l + (r - l) / 2;
+    if (arr[mid] < value) {
+      l = mid + 1;
+    } else if (arr[mid] > value) {
+      r = mid - 1;
+    } else {
+      count++;
+      int temp = mid;
+      while (temp > 0 && arr[temp - 1] == value) {
+        count++;
+        temp--;
+      }
+      temp = mid;
+      while (temp < size - 1 && arr[temp + 1] == value) {
+        count++;
+        temp++;
+      }
+      break;
+    }
+  }
+  return count;
 }
